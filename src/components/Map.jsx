11e266d0d87e0a96719a7e3880bd7a014675b25e
PV5667 <reachpv05@gmail.com>
2023-05-05 22:04:51 -0700

    plotting for solar panels done

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { useState } from 'react';
 import DeckGL from '@deck.gl/react';
 import { Draw90DegreePolygonMode, DrawPolygonByDraggingMode, ViewMode, DrawRectangleUsingThreePointsMode, DrawRectangleMode } from '@nebula.gl/edit-modes';
-import { EditableGeoJsonLayer } from '@nebula.gl/layers';
+import { EditableGeoJsonLayer} from '@nebula.gl/layers';
+import {GeoJsonLayer, PolygonLayer} from '@deck.gl/layers';
 import StaticMap from 'react-map-gl';
 import {Box, Button, Grid} from "@mantine/core"
 import { MapView } from '@deck.gl/core';
 import { bboxPolygon, area, bbox, squareGrid } from '@turf/turf';
+import * as fs from "fs";
+
+
+const polygonData = {}
+
 
 const box = [-122.1404, 37.7139, -122.1346, 37.7195];
 //const bbox = [37.3602, -121.9742, 37.646, -121.9686];
@@ -19,35 +25,56 @@ console.log(poly);
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZ2Vvcmdpb3MtdWJlciIsImEiOiJjanZidTZzczAwajMxNGVwOGZrd2E5NG90In0.gdsRu_UeU_uPi9IulBruXA';
 
 const INITIAL_VIEW_STATE = {
-  longitude: -121.89,
-  latitude: 37.3387,
-  zoom: 13,
+  longitude: -120.4265,
+  latitude: 34.8670892225,
+  zoom: 16,
   pitch: 0,
   bearing: 0,
 };
 function Map () {
   const [features, setFeatures] = useState({
     type: "FeatureCollection",
-    features: []
+    features: []//panels.features,
   });
-  const [mode, setMode] = useState(() => DrawRectangleUsingThreePointsMode); 
+  const [mode, setMode] = useState(() => DrawRectangleMode); 
   const [selectedFeatureIndexes, setSelectedFeatureIndexes] = useState(
     []
   );
 
-  const layer = new EditableGeoJsonLayer({
+  const panelLayer = new PolygonLayer({
+    id: "poly-layers",
+    data: polygonData.features,
+    pickable: true,
+    //stroked: true,
+    filled: true,
+    extruded: false,
+    wireframe: true,
+    //lineWidthMinPixels: 1,
+    getPolygon: d => d.geometry.coordinates,
+    //getLineColor: [80, 80, 80],
+    getFillColor: [60, 255, 80],
+    //getLineWidth: 1
+  });
+
+  const editableGjsonLayer = new EditableGeoJsonLayer({
     id: 'geojson-layer',
     data: features,
-    mode: DrawRectangleUsingThreePointsMode,
+    mode: DrawRectangleMode, 
     selectedFeatureIndexes: selectedFeatureIndexes,
     onEdit: ({ updatedData }) => {
       console.log(updatedData);
-      const newData = squareGrid(bbox(updatedData), 0.5);
-      console.log("New Data ")
+      const newData = squareGrid(bbox(updatedData), 0.1);
+      console.log("New Data:")
       console.log(newData);
       setFeatures(newData);
+      saveSelection(newData);
     }
   });
+
+  function saveSelection (newData) {
+    const data = JSON.stringify(newData);
+    console.log(newData);
+  }
 
   return (
     <div class="my-12">
@@ -61,12 +88,13 @@ function Map () {
             doubleClickZoom: false
           }}
           id="deck-gl"
-          layers={[layer]}
-          getCursor={layer.getCursor.bind(layer)}
+          layers={[panelLayer, editableGjsonLayer]} 
+          getCursor={editableGjsonLayer.getCursor.bind(editableGjsonLayer)}
         >
         <MapView id="map" controller={false} width="100%" height="100%">
           <StaticMap mapStyle="mapbox://styles/mapbox/satellite-v9" mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
         </MapView>
+        
         </DeckGL>
       </Box>
     </div>
