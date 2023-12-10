5992c37645f8a4f6d06f6b3a969912796624a1bc
PV5667 <reachpv05@gmail.com>
2023-12-09 23:09:29 -0600

    add clustering, fix multiple small bugs

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,16 +3,17 @@ import { useState, useRef, useContext, useEffect } from 'react';
 import DeckGL from '@deck.gl/react';
 import { Draw90DegreePolygonMode, DrawPolygonByDraggingMode, DrawPolygonMode, ViewMode, DrawRectangleUsingThreePointsMode, DrawRectangleMode } from '@nebula.gl/edit-modes';
 import { EditableGeoJsonLayer} from '@nebula.gl/layers';
-import {GeoJsonLayer, PolygonLayer} from '@deck.gl/layers';
+import {GeoJsonLayer, PolygonLayer, IconLayer} from '@deck.gl/layers';
 import StaticMap from 'react-map-gl';
 import {Box, Button, Grid, Stack, Center, Title, Switch, Group, Loader, Modal, SegmentedControl, Progress, CloseButton} from "@mantine/core"
-import { MapView, FlyToInterpolator } from '@deck.gl/core';
+import { MapView, FlyToInterpolator, WebMercatorViewport } from '@deck.gl/core';
 import { bboxPolygon, area, bbox, buffer, squareGrid, lineString} from '@turf/turf';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import {LocationContext} from './LocationProvider';
 import { FeaturesContext } from './FeaturesProvider';
 import { useDisclosure } from '@mantine/hooks';
 import { WorldDownload, Trash } from 'tabler-icons-react';
+import Supercluster from 'supercluster';
 
 
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoicHY1NjY3IiwiYSI6ImNsZGFtOHVoejBiZ2Mzb3A2djgyaDl1OGEifQ.FSssERk7wLiG1fDpen0iXA';
@@ -39,6 +40,12 @@ function Map () {
     []);
   const [numPanelsFound, setNumPanelsFound] = useState(0);
   const [progress, setProgress] = useState(0);
+  const [clusters, setClusters] = useState({
+    type: "FeatureCollection",
+    features: []
+  }); 
+  const [useClusters, setUseClusters] = useState(false);
+  const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
   const location = useContext(LocationContext)[0];
   //console.log(location)
   const constructedViewState = {
@@ -81,29 +88,30 @@ function Map () {
         setAwaitingResponse(false);
           response.json()
           .then(function(response) {
-              //console.log(typeof(JSON.parse(response)));
-              const newPanels = JSON.parse(response).features;
+              console.log(typeof(JSON.parse(response)));
+              const newPanels = JSON.parse(response).features
               setPanels(newPanels);
               setNumPanelsFound(newPanels.length);
               setFeatures({
                 type: "FeatureCollection",
                 features: []
               });
-              //console.log(newPanels);
+              console.log(newPanels);
               setMode(() => ViewMode);
               setAnalysisMode("view");
+              handleViewStateChange(viewState, newPanels);
           });
       }
       else {
           setAwaitingResponse(false);
-          //console.log(response);
+          console.log(response);
           errorHandlers.open();
           throw Error('Something went wrong');
       }
   })
   .catch(error => {
     setAwaitingResponse(false);
-    //console.log(error);
+    console.log(error);
     errorHandlers.open();
     throw Error('Something went wrong');
   });
@@ -141,21 +149,98 @@ function Map () {
         setMode(() => ViewMode);
       }
   }
+
+  function getClustersToPlot(zoom, bounds, newPanels) {
+    //console.log("Bounds: ", bounds)
+    //console.log("Zoom: ", zoom)
+    let panelsToPlot;
+    if (newPanels.length > 0) {
+      panelsToPlot = newPanels;
+    } else {
+      panelsToPlot = panels;
+    }
+    console.log("Inside Get Clusters: " + panelsToPlot.length)
+    if (panelsToPlot.length == 0) {
+      return [];
+    }
+    const index = new Supercluster({ radius: 60, maxZoom: 16 });
+    index.load(
+      panelsToPlot.map((d) => ({
+        type: 'Feature',
+        geometry: { 
+          type: 'Point',
+          coordinates: [d.geometry.coordinates[0][0][0], d.geometry.coordinates[0][0][1]],
+        },
+        properties: d.properties,
+      }))
+    );
+    const clusters = index.getClusters(bounds, Math.floor(zoom));
+    //console.log(clusters)
+    return clusters;
+  }
+  // from https://github.com/visgl/deck.gl/blob/8.9-release/examples/website/icon/icon-cluster-layer.js
+  function getIconName(size) {
+    if (size === 0) {
+      return '';
+    }
+    if (size < 10) {
+      return `marker-${size}`;
+    }
+    if (size < 100) {
+      return `marker-${Math.floor(size / 10)}0`;
+    }
+    return 'marker-100';
+  }
   
-  const panelLayer = new PolygonLayer({
-    id: "poly-layers",
-    data: panels,
-    pickable: true,
-    filled: true,
-    extruded: true,
-    wireframe: true,
-    getPolygon: d => d.geometry.coordinates,
-    getFillColor: [255, 0, 0],
-    getLineColor: [168, 85, 247],
-    getLineWidth: 0.3,
-    getElevation: 1,
-    _normalize: true,
-  });
+  function getIconSize(size) {
+    return Math.min(100, size) / 100 + 1;
+  }
+
+  const handleViewStateChange = (viewState, newPanels=[]) => {
+    setViewState(viewState)
+    const viewport = new WebMercatorViewport(viewState);
+    const nw = viewport.unproject([0, 0]);
+    const se = viewport.unproject([viewport.width, viewport.height]);
+    var bounds = [nw[0], se[1], se[0], nw[1]];
+    var zoom = viewState.zoom;
+    if (zoom <= 16) {
+      const clusters = getClustersToPlot(zoom, bounds, newPanels);
+      setUseClusters(true);
+      setClusters(clusters);      
+    } else {
+      setUseClusters(false);
+    }
+  };
+  // switch between polygon and icon layer based on boolean value in state
+  let featureLayer;
+  if (useClusters) {
+    featureLayer = new IconLayer({
+      id: "icon-layer",
+      data: clusters,
+      pickable: false,
+      iconAtlas: "./src/assets/location-icon-atlas.png",
+      iconMapping: "https://raw.githubusercontent.com/visgl/deck.gl/8.9-release/examples/website/icon/data/location-icon-mapping.json",
+      getIcon: d => getIconName(d.properties.cluster ? d.properties.point_count : 1),
+      getSize: d => getIconSize(d.properties.cluster ? d.properties.point_count : 1),
+      getPosition: d => d.geometry.coordinates,
+      sizeScale: 40,
+    })
+  } else {
+    featureLayer = new PolygonLayer({
+      id: "poly-layers",
+      data: panels,
+      pickable: true,
+      filled: true,
+      extruded: true,
+      wireframe: true,
+      getPolygon: d => d.geometry.coordinates,
+      getFillColor: [255, 0, 0],
+      getLineColor: [168, 85, 247],
+      getLineWidth: 0.3,
+      getElevation: 1,
+      _normalize: true,
+    })
+  }
   
 
   const editableGjsonLayer = new EditableGeoJsonLayer({
@@ -164,10 +249,10 @@ function Map () {
     mode: mode, 
     selectedFeatureIndexes: selectedFeatureIndexes,
     onEdit: ({ updatedData }) => {
-      //console.log(updatedData);
+      console.log(updatedData);
       const options = { units: "kilometers", mask: updatedData};
       const newData = squareGrid(bbox(buffer(updatedData, 0.06)), 0.12, options);
-      //console.log("New Data:")
+      console.log("New Data:")
       console.log(newData);
       if (newData.features.length > 900) {
         //console.log(newData.features.length)
@@ -178,7 +263,7 @@ function Map () {
     }
   });
 
-  let interval; // Declare the interval variable
+  let interval; 
 
   useEffect(() => {
     if (awaitingResponse) {
@@ -187,12 +272,13 @@ function Map () {
           if (prev === "Almost Done...") {
             return "Almost Done...";
           }
-          const newProgress = prev + Math.round(100 / features.features.length);
+          const newProgress = +((prev + (100 / features.features.length)).toFixed(2));
+          console.log("Working...." + newProgress);
           return newProgress >= 100 ? "Almost Done..." : newProgress; 
         });
       }, 2005);
     } else {
-      //console.log("Clearing Interval");
+      console.log("Clearing Interval");
       setProgress(0); 
       clearInterval(interval);
     }
@@ -209,7 +295,6 @@ function Map () {
       clearInterval(interval);
     }
   }, [progress, awaitingResponse]);
-
   return (
     <>
     <Modal size="xl" opened={errorOpened} onClose={errorHandlers.close} withCloseButton={false} centered>
@@ -219,7 +304,7 @@ function Map () {
     </Modal>
     <Modal size="xl" opened={sizeOpened} onClose={sizeHandlers.close} withCloseButton={false} centered>
         <Group position="center" direction="row" spacing="xs">
-        Oops! Please reduce the size of your selection.
+        Please reduce the size of your selection.
         </Group>
     </Modal>
     <div>
@@ -233,9 +318,10 @@ function Map () {
           initialViewState={constructedViewState}
           controller={{doubleClickZoom: false}}
           id="deck-gl"
-          layers={[panelLayer, editableGjsonLayer]} 
+          layers={[featureLayer, editableGjsonLayer]} 
           getCursor={editableGjsonLayer.getCursor.bind(editableGjsonLayer)}
           getTooltip={mode===ViewMode ? ({object}) => object && `Area: ${area(object).toFixed(3)} m²\nLon: ${object.geometry.coordinates[0][0][0].toFixed(4)}\nLat: ${object.geometry.coordinates[0][0][1].toFixed(4)}`: null}
+          onViewStateChange={ e => handleViewStateChange(e.viewState) }
         >
         <MapView id="map" controller={false} width="100%" height="100%">
             <StaticMap mapStyle="mapbox://styles/mapbox/satellite-v9" mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
@@ -247,9 +333,10 @@ function Map () {
         <Title order={4} c="white">Area: {calculateArea(features)} km²</Title>
         <button type="button" class="inline-flex items-center py-2 text-white 
         text-md font-medium rounded-md gap-1 hover:text-red-500 p-1 outline focus:outline-none focus:ring focus:ring-red" 
-        onClick={() => setFeatures({type: "FeatureCollection",
-        features: []
-        })}>
+        onClick={() => {
+          setFeatures({type: "FeatureCollection", features: []})
+          setAwaitingResponse(false);
+          }}>
           Clear Selection
           <Trash size={20} />
         </button>
